ae75b3ac7f588a13f511937a1e8a76ee1eddf12d
Edrick Leong <10529706+edrickleong@users.noreply.github.com>
2023-05-07 18:05:18 +0800

    feat: add validation with zod

--- src/app/age-calculator/_components/card.tsx
+++ src/app/age-calculator/_components/card.tsx
@@ -1,14 +1,10 @@
 "use client"
 import arrow from "/public/icon-arrow.svg"
 import Image from "next/image"
-import { useForm } from "react-hook-form"
-import { useState } from "react"
-
-type SimpleDate = {
-  day: number
-  month: number
-  year: number
-}
+import { FieldError, useForm } from "react-hook-form"
+import { useMemo, useState } from "react"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 type Age = {
   years: number
@@ -16,17 +12,58 @@ type Age = {
   days: number
 }
 
+const ageSchema = z
+  .object({
+    day: z
+      .number({
+        required_error: "This field is required",
+        invalid_type_error: "Must be a valid day",
+      })
+      .min(1, { message: "Must be a valid day" }),
+    month: z
+      .number({
+        required_error: "This field is required",
+        invalid_type_error: "Must be a valid month",
+      })
+      .min(1, { message: "Must be a valid month" })
+      .max(12, { message: "Must be a valid month" }),
+    year: z
+      .number({
+        required_error: "This field is required",
+        invalid_type_error: "Must be a valid year",
+      })
+      .min(1900, { message: "Must be from 1900" })
+      .max(new Date().getFullYear(), { message: "Must be in the past" }),
+  })
+  .refine(
+    (obj) => {
+      const daysInMonth = new Date(obj.year, obj.month, 0).getDate()
+      return obj.day >= 1 && obj.day <= daysInMonth
+    },
+    {
+      message: "Must be a valid date",
+      path: ["day"],
+    }
+  )
+
 export function Card() {
-  const { register, handleSubmit } = useForm<SimpleDate>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(ageSchema),
+  })
   const [age, setAge] = useState<Age>()
+
+  const today = useMemo(() => new Date(), [])
 
   return (
     <form
-      className="max-w-[840px] w-full rounded-3xl rounded-br-[96px] bg-white px-6 pb-12 pt-[52px]"
+      className="w-full max-w-[840px] rounded-3xl rounded-br-[96px] bg-white px-6 pb-12 pt-[52px]"
       onSubmit={handleSubmit((it) => {
         const birthDate = new Date(it.year, it.month - 1, it.day)
-        const now = new Date()
-        const { years, months, days } = getAge(birthDate, now)
+        const { years, months, days } = getAge(birthDate, today)
         setAge({ years, months, days })
       })}
     >
@@ -34,46 +71,80 @@ export function Card() {
         <div className="flex flex-col">
           <label
             htmlFor="day"
-            className="text-sm font-semibold uppercase text-[--smokey-grey]"
+            className={`text-sm font-semibold uppercase text-[--smokey-grey] ${
+              errors.day && "text-[--light-red]"
+            }`}
           >
             Day
           </label>
           <input
-            className="mt-2 h-[54px] w-full max-w-[160px] rounded border border-[--light-grey] p-4 font-bold outline-purple"
+            type="number"
+            className={`mt-2 h-[54px] w-full max-w-[160px] rounded border border-[--light-grey] p-4 font-bold outline-purple ${
+              errors.day && "border-[--light-red] "
+            }`}
             placeholder="DD"
             {...register("day", { valueAsNumber: true })}
           />
+          {errors.day && (
+            <span className="mt-2 text-sm text-[--light-red]">
+              {(errors.day as FieldError).message}
+            </span>
+          )}
         </div>
         <div className="flex flex-col">
           <label
             htmlFor="month"
-            className="w-full text-sm font-semibold uppercase text-[--smokey-grey]"
+            className={`w-full text-sm font-semibold uppercase text-[--smokey-grey] ${
+              errors.month && "text-[--light-red]"
+            }`}
           >
             Month
           </label>
           <input
-            className="mt-2 h-[54px] w-full max-w-[160px] rounded border border-[--light-grey] p-4 font-bold outline-purple"
+            className={`mt-2 h-[54px] w-full max-w-[160px] rounded border border-[--light-grey] p-4 font-bold outline-purple ${
+              errors.month && "border-[--light-red] "
+            }`}
             placeholder="MM"
             {...register("month", { valueAsNumber: true })}
           />
+          {errors.month && (
+            <span className="mt-2 text-sm text-[--light-red]">
+              {(errors.month as FieldError).message}
+            </span>
+          )}
         </div>
         <div className="flex flex-col">
           <label
             htmlFor="year"
-            className="w-full text-sm font-semibold uppercase text-[--smokey-grey]"
+            className={
+              `w-full text-sm font-semibold uppercase text-[--smokey-grey] ${errors.year && "text-[--light-red]"}`
+            }
           >
             Year
           </label>
           <input
-            className="mt-2 h-[54px] w-full max-w-[160px] rounded border border-[--light-grey] p-4 font-bold outline-purple"
+            className={`mt-2 h-[54px] w-full max-w-[160px] rounded border border-[--light-grey] p-4 font-bold outline-purple ${
+              errors.year && "border-[--light-red] "
+            }`}
             placeholder="YYYY"
             {...register("year", { valueAsNumber: true })}
           />
+          {errors.year && (
+            <span className="mt-2 text-sm text-[--light-red]">
+              {(errors.year as FieldError).message}
+            </span>
+          )}
         </div>
       </div>
       <div className="relative mt-12 h-[1px] w-full bg-[--light-grey]">
         <button className="absolute bottom-0 right-1/2 flex h-16 w-16 translate-x-1/2 translate-y-1/2 items-center justify-center rounded-full bg-purple outline-offset-2 outline-purple hover:bg-purple/90 sm:right-0 sm:h-24 sm:w-24 sm:translate-x-0">
-          <Image src={arrow} alt="" height={36} width={36} className="h-6 w-6 sm:w-9 sm:h-9" />
+          <Image
+            src={arrow}
+            alt=""
+            height={36}
+            width={36}
+            className="h-6 w-6 sm:h-9 sm:w-9"
+          />
         </button>
       </div>
       <div className="mt-[68px] text-5xl font-bold italic">
