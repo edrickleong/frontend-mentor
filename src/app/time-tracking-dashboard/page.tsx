47072bf7c105223fc31b494c7a91888d42b18441
Edrick Leong <10529706+edrickleong@users.noreply.github.com>
2023-05-09 18:36:41 +0800

    wip: time tracking dashboard

--- src/app/time-tracking-dashboard/page.tsx
+++ src/app/time-tracking-dashboard/page.tsx
@@ -3,14 +3,13 @@ import { rubik } from "@/app/styles/fonts"
 import jeremy from "#/time-tracking-dashboard/image-jeremy.png"
 import Image from "next/image"
 import data from "./data.json"
-import { useState } from "react"
+import React from "react"
+import * as Tabs from "@radix-ui/react-tabs"
 
 export default function Page() {
-  const [timeframe, setTimeframe] = useState("weekly")
-
   return (
     <div
-      className={`flex min-h-screen w-full flex-col items-center bg-[--very-dark-blue] px-6 pt-20 ${rubik.className}`}
+      className={`flex min-h-screen w-full flex-col items-center bg-[--very-dark-blue] px-6 py-20 ${rubik.className}`}
       style={
         {
           "--blue": "hsl(246, 80%, 60%)",
@@ -27,7 +26,10 @@ export default function Page() {
         } as React.CSSProperties
       }
     >
-      <div className="flex w-full max-w-[375px] flex-col gap-6">
+      <Tabs.Root
+        className="flex w-full max-w-[375px] flex-col gap-6"
+        defaultValue="weekly"
+      >
         <div className="w-full rounded-xl bg-[--dark-blue] text-white">
           <div className="flex flex-row items-center gap-4 rounded-xl bg-[--blue] p-8">
             <div className="h-16 w-16 rounded-full border-4 border-white">
@@ -38,49 +40,74 @@ export default function Page() {
               <div className="text-2xl">Jeremy Robson</div>
             </div>
           </div>
-          <div className="flex flex-row justify-between p-8">
-            <div>Daily</div>
-            <div>Weekly</div>
-            <div>Monthly</div>
-          </div>
+          <Tabs.List className="flex flex-row justify-between text-xl p-8">
+            <Tabs.Trigger
+              value="daily"
+              className="data-[state=active]:text-white text-[--desaturated-blue]"
+            >
+              Daily
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="weekly"
+              className="data-[state=active]:text-white text-[--desaturated-blue]"
+            >
+              Weekly
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="monthly"
+              className="data-[state=active]:text-white text-[--desaturated-blue]"
+            >
+              Monthly
+            </Tabs.Trigger>
+          </Tabs.List>
         </div>
-        {data.map((it) => (
-          <div
-            key={it.title}
-            className={`w-full rounded-xl ${getBackground(it.title)} pt-8`}
-          >
-            <div className="flex flex-row justify-between rounded-xl bg-[--dark-blue] p-8 text-white">
-              <div>
-                <div className="text-lg font-medium">{it.title}</div>
-                <div className="text-3xl font-light">
-                  {timeframe === "daily"
-                    ? it.timeframes.daily.current
-                    : timeframe === "weekly"
-                    ? it.timeframes.weekly.current
-                    : it.timeframes.monthly.current}
-                  hrs
+        <div>
+          {["daily", "weekly", "monthly"].map((timeframe) => (
+            <Tabs.Content
+              value={timeframe}
+              key={timeframe}
+              className="flex flex-col gap-6"
+            >
+              {data.map((it) => (
+                <div
+                  key={it.title}
+                  className={`w-full rounded-xl ${background(it.title)} pt-8 overflow`}
+                >
+                  <div className="flex flex-row justify-between rounded-xl bg-[--dark-blue] p-8 text-white">
+                    <div>
+                      <div className="text-lg font-medium">{it.title}</div>
+                      <div className="text-3xl font-light">
+                        {timeframe === "daily"
+                          ? it.timeframes.daily.current
+                          : timeframe === "weekly"
+                          ? it.timeframes.weekly.current
+                          : it.timeframes.monthly.current}
+                        hrs
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-end justify-end gap-2">
+                      <div className="text-sm text-[--pale-blue]">
+                        {`${
+                          timeframe === "daily"
+                            ? `Last Week - ${it.timeframes.weekly.previous} hours`
+                            : timeframe === "weekly"
+                            ? `Yesterday - ${it.timeframes.daily.previous} hours`
+                            : `Last Month - ${it.timeframes.monthly.previous} hours`
+                        }`}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col items-end justify-end gap-2">
-                <div className="text-sm text-[--pale-blue]">
-                  {`${
-                    timeframe === "weekly"
-                      ? `Last Week - ${it.timeframes.weekly.previous} hours`
-                      : timeframe === "daily"
-                      ? `Yesterday - ${it.timeframes.daily.previous} hours`
-                      : `Last Month - ${it.timeframes.monthly.previous} hours`
-                  }`}
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+              ))}
+            </Tabs.Content>
+          ))}
+        </div>
+      </Tabs.Root>
     </div>
   )
 }
 
-function getBackground(it: string) {
+function background(it: string) {
   switch (it) {
     case "Work":
       return "bg-[--light-red]"
